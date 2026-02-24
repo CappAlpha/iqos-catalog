9bcd3ec71a4e12904b3f937fd5a669091b6fb7a4
CappAlpha <egor.lapin.workspace@gmail.com>
2026-02-24 20:31:02 +0300

    Fixes

--- src/modules/catalog/features/ui/ProductCard/ProductCard.tsx
+++ src/modules/catalog/features/ui/ProductCard/ProductCard.tsx
@@ -1,9 +1,11 @@
 import cn from "classnames";
-import s from "./ProductCard.module.scss";
-import { Button } from "../../../../../shared/ui/Button";
-import type { Product } from "../../model/types";
-import { cartStore } from "../../../../cart/features/model/cartStore";
 import { observer } from "mobx-react-lite";
+
+import { Button } from "../../../../../shared/ui/Button";
+import { cartStore } from "../../../../cart/features/model/cartStore";
+import type { Product } from "../../model/types";
+
+import s from "./ProductCard.module.scss";
 
 interface Props {
   product: Product;
@@ -16,36 +18,35 @@ export const ProductCard = observer(({ product }: Readonly<Props>) => {
   const isAddToCartLoading = getItemAction(product.id) === "add";
 
   return (
-    <div
-      className={cn(s.root)}
-    >
+    <div className={cn(s.root)}>
       <div className={s.imgWrap}>
-        {pictureUrl ? <img
-          className={s.img}
-          src={pictureUrl}
-          alt={name}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-          : (
-            <div className={s.placeholder} aria-hidden="true">
-              Ошибка загрузки картинки
-            </div>
-          )}
+        {pictureUrl ? (
+          <img
+            className={s.img}
+            src={pictureUrl}
+            alt={name}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        ) : (
+          <div className={s.placeholder} aria-hidden="true">
+            Ошибка загрузки картинки
+          </div>
+        )}
       </div>
 
       <div className={s.textWrap}>
         <h5 className={s.title}>{name}</h5>
-        {categoryTitle &&
-          <p className={s.category}>{categoryTitle}</p>
-        }
+        {categoryTitle && <p className={s.category}>{categoryTitle}</p>}
       </div>
 
       <div className={s.bottom}>
         <div className={s.bottomWrap}>
-          <b className={s.price}>
-            {price} &#8381;
-          </b>
-          <Button className={s.button} loading={isAddToCartLoading} onClick={() => addToCart(product)}>
+          <b className={s.price}>{price} &#8381;</b>
+          <Button
+            className={s.button}
+            loading={isAddToCartLoading}
+            onClick={() => addToCart(product)}
+          >
             Добавить
           </Button>
         </div>
