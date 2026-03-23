0afff8301c3e4b9d6815e6055848aeca75829ff6
CappAlpha <egor.lapin.workspace@gmail.com>
2026-03-23 19:36:00 +0300

    Add svgr

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as LogoIqos } from "./header/logo-iqos.svg";
+export { default as LogoLil } from "./header/logo-lil.svg";
+export { default as CatalogIcon } from "./header/catalog.svg";
+export { default as BluetoothIcon } from "./header/bluetooth.svg";
+export { default as CartIcon } from "./header/cart.svg";
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ include: "**/*.svg" })],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/app/styles/_mixins.scss" as *;`
+        additionalData: `@use "@/app/styles/_mixins.scss" as *;`,
       },
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-})
+});
