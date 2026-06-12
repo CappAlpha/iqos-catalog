db42d6fad3b0a7ada39983363e346293108cb9bc
CappAlpha <egor.lapin.workspace@gmail.com>
2026-06-12 16:06:00 +0300

    Fix usb

--- src/modules/usb/features/ui/DeviceMetadataSection/DeviceMetadataSection.tsx
+++ src/modules/usb/features/ui/DeviceMetadataSection/DeviceMetadataSection.tsx
@@ -9,6 +9,7 @@ export interface Props {
 }
 
 export const DeviceMetadataSection = observer(({ device }: Props) => {
+  const { manufacturerName, productName, vendorId, productId } = device;
   const battery = usbM.batteryLevel;
 
   return (
@@ -16,16 +17,16 @@ export const DeviceMetadataSection = observer(({ device }: Props) => {
       <h3 className={s.title}>Информация об устройстве:</h3>
       <div className={s.wrap}>
         <p className={s.manufacturer}>
-          <b>Производитель:</b> {device.manufacturerName ?? "Неизвестно"}
+          <b>Производитель:</b> {manufacturerName ?? "Неизвестно"}
         </p>
         <p className={s.product}>
-          <b>Продукт:</b> {device.productName ?? "Неизвестно"}
+          <b>Продукт:</b> {productName ?? "Неизвестно"}
         </p>
         <p className={s.vendorId}>
-          <b>Vendor ID:</b> {device.vendorId}
+          <b>Vendor ID:</b> {vendorId}
         </p>
         <p className={s.productId}>
-          <b>Product ID:</b> {device.productId}
+          <b>Product ID:</b> {productId}
         </p>
         {battery && (
           <p className={s.battery}>
