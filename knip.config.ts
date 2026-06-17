cf9821079605728a1b31d23790706b1e515fb465
CappAlpha <egor.lapin.workspace@gmail.com>
2026-06-17 19:12:24 +0300

    Fix

--- knip.config.ts
+++ knip.config.ts
@@ -3,5 +3,5 @@ import type { KnipConfig } from "knip";
 export default {
   ignore: ["src/global.d.ts", "src/app/styles/**", "src/shared/hooks/**"],
   exclude: ["types"],
-  ignoreDependencies: ["@capacitor/ios", "zod-validation-error"],
+  ignoreDependencies: ["zod-validation-error"],
 } satisfies KnipConfig;
