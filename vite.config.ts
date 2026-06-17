58894239efed331355bc889047f5cb92a1f57fa1
CappAlpha <egor.lapin.workspace@gmail.com>
2026-06-17 15:35:22 +0300

    Add babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig, type CSSOptions } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -7,7 +8,15 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig(({ mode }) => {
   return {
     base: mode === "capacitor" ? "./" : "/iqos-catalog/",
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      babel({
+        presets: [reactCompilerPreset()],
+        include: /\.(jsx|tsx|js|ts)$/,
+        exclude: /node_modules/,
+      }),
+    ],
     css: {
       preprocessorOptions: {
         scss: {
@@ -16,6 +25,32 @@ export default defineConfig(({ mode }) => {
         },
       },
     } as CSSOptions,
+    build: {
+      target: "es2020",
+      minify: true,
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            const assetName = assetInfo.names?.[0] ?? "";
+
+            const isImage = /\.(png|jpe?g|svg|gif|tiff|bmp|ico|webp)$/i.test(
+              assetName,
+            );
+            const isFont = /\.(woff|woff2|eot|ttf|otf)$/i.test(assetName);
+
+            if (isImage) {
+              return `img/[name][extname]`;
+            }
+            if (isFont) {
+              return `fonts/[name][extname]`;
+            }
+            return `[name][extname]`;
+          },
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+        },
+      },
+    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
