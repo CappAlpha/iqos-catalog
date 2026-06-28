252d0dae10681e3d4d6944f2a9e6177565697176
CappAlpha <egor.lapin.workspace@gmail.com>
2026-06-29 00:02:41 +0300

    Add show products btn in history

--- src/modules/cart/features/ui/HistoryCard/HistoryCard.tsx
+++ src/modules/cart/features/ui/HistoryCard/HistoryCard.tsx
@@ -1,7 +1,9 @@
 import cn from "classnames";
 import { observer } from "mobx-react-lite";
+import { useState } from "react";
 
 import { formatPrice } from "@/shared/lib/formatPrice";
+import { Button } from "@/shared/ui/Button";
 
 import { cartM } from "../../model/cartM";
 import type { Order } from "../../model/types";
@@ -13,6 +15,8 @@ interface Props {
 }
 
 export const HistoryCard = observer(({ order }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div
       className={cn(
@@ -20,7 +24,7 @@ export const HistoryCard = observer(({ order }: Props) => {
         cartM.getItemStatus(order.id).isAddLoading && s.cardIntro,
       )}
     >
-      <div className={s.header}>
+      <div className={s.left}>
         <p className={s.title}>
           <b>Заказ #{order.id}</b>
         </p>
@@ -28,12 +32,33 @@ export const HistoryCard = observer(({ order }: Props) => {
           {new Date(order.date).toLocaleDateString("ru-RU")}
         </p>
       </div>
-      <div className={s.total}>
-        <p>
-          <b>
-            Сумма: <span>{formatPrice(order.totalPrice)}</span>
-          </b>
-        </p>
+
+      <div className={s.right}>
+        <div className={s.total}>
+          <p>
+            <b>
+              Сумма: <span>{formatPrice(order.totalPrice)}</span>
+            </b>
+          </p>
+        </div>
+        <Button
+          className={s.detailsBtn}
+          color="outline"
+          onClick={() => setExpanded((prev) => !prev)}
+        >
+          {expanded ? "Свернуть" : "Подробнее"}
+        </Button>
+      </div>
+
+      <div className={cn(s.itemsWrapper, expanded && s.itemsWrapperOpen)}>
+        <div className={s.items}>
+          {order.items.map(({ product: { name, id }, quantity }) => (
+            <div key={id} className={s.item}>
+              <span>{name}</span>
+              <span>x{quantity}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
