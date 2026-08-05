fc5bc01a730bf74117abe0b29c05633d90cef919
CappAlpha <egor.lapin.workspace@gmail.com>
2026-08-05 20:41:44 +0300

    Fix

--- src/shared/ui/ApkUpdaterToast/LazyApkUpdaterToast.tsx
+++ src/shared/ui/ApkUpdaterToast/LazyApkUpdaterToast.tsx
@@ -1,11 +1,9 @@
 import { lazy, Suspense } from "react";
 
-import { IS_ANDROID } from "@/shared/config/platform";
+import { IS_ANDROID, IS_CAPACITOR } from "@/shared/config/platform";
 
 const ApkUpdaterToast =
-  import.meta.env.MODE === "capacitor" && IS_ANDROID
-    ? lazy(() => import("./ApkUpdaterToast"))
-    : null;
+  IS_CAPACITOR && IS_ANDROID ? lazy(() => import("./ApkUpdaterToast")) : null;
 
 export function LazyApkUpdaterToast() {
   if (!ApkUpdaterToast) return null;
